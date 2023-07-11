861ef039477e01c5cbaaa1054a1e1e874af8a209
deliahong <delia.hong@soohoobook.com>
2023-07-11 17:36:34 +0800

    update

---
+++ EFcoreInheritance/EnergyHero.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFcoreInheritance
+{
+    public class EnergyHero : Hero
+    {
+        public int Energy { get; set; }
+    }
+
+    public class EnerhyHeroConfiguration : IEntityTypeConfiguration<EnergyHero>
+    {
+        public void Configure(EntityTypeBuilder<EnergyHero> builder)
+        {
+            builder.ToTable(nameof(EnergyHero));
+        }
+    }
+}
---
+++ EFcoreInheritance/TestContext.cs
@@ -0,0 +1,27 @@
+﻿using EFcoreInheritance;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System.Reflection;
+
+namespace EFcoreConcurrency
+{
+    public class TestContext : DbContext
+    {
+        public DbSet<Hero> Heroes { get; set; }
+        public DbSet<ManaHero> ManaHeroes { get; set; }
+        public DbSet<EnergyHero> EnergyHeroes { get; set; }
+        
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=EFInheritance;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False")
+                .LogTo((message) => Console.WriteLine("【TestContext:】" + message + "\r\n"), LogLevel.Information)
+                ;
+        }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+            base.OnModelCreating(builder);
+        }
+    }
+}
